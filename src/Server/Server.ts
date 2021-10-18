0d6ea71d29f69c988266572f0254a06cc56e6f48
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-18 15:34:28 +1100

    login handler

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,4 +1,5 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { LoginHandler } from './LoginHandler';
 import { Utils } from './Utils';
 
 export class Server {
@@ -11,6 +12,16 @@ export class Server {
             (req: IncomingMessage, res: ServerResponse  )=> {
                 console.log(`got request from:${req.url}`);
                 const basePath = Utils.getUrlBasePath(req.url);
+
+                switch (basePath) {
+                    case 'login':
+                        new LoginHandler(req, res).handleRequest();
+                        break;
+                
+                    default:
+                        break;
+                }
+
                 res.end();
             }
         ).listen(8080)
