e3add2c18295cfc003f58d5e54ffa56f62b0d7ee
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-19 10:11:54 +1100

    addint authorization

--- src/Server/LoginHandler.ts
+++ src/Server/LoginHandler.ts
@@ -1,20 +1,27 @@
 import { IncomingMessage, ServerResponse } from "http"
 import { resolve } from "path/posix";
-import { Account } from "./Model";
+import { Account, TokenGenerator } from "./Model";
 
 export class LoginHandler {
     private req: IncomingMessage;
     private res: ServerResponse;
+    private tokenGenerator: TokenGenerator;
 
-    public constructor(req: IncomingMessage, res: ServerResponse){
+    public constructor(req: IncomingMessage, res: ServerResponse, tokenGenerator: TokenGenerator){
         this.req = req;
         this.res = res;
+        this.tokenGenerator = tokenGenerator;
     }
 
     public  async handleRequest():Promise<void> {
        const body = await  this.getRequestBody();
-       console.log(`request username:${body.username}`)
-       console.log(`request password:${body.password}`)
+       const sessionToken = await this.tokenGenerator.generateToken(body);
+       if (sessionToken) {
+           this.res.write('valid credentials');
+       }else {
+           this.res.write('wrong credentials');
+       }
+
     }
 
     private async getRequestBody():Promise<Account> {
