56c16269849a7735944a9bc641f18a8bf8f4b715
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-18 14:58:57 +1100

    starting the server

---
+++ src/Server/Server.ts
@@ -0,0 +1,17 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+export class Server {
+
+    /**
+     * createServer
+     */
+    public createServer() {
+        createServer(
+            (req: IncomingMessage, res: ServerResponse  )=> {
+                console.log(`got request from:${req.url}`);
+                res.end();
+            }
+        ).listen(8080)
+        console.log('server started')
+    }
+}
