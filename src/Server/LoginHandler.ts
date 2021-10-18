a9e5a51dbf5273bcd15cd734b11b965b71c7a805
Michael Pappas <mixalis.pappas23@gmail.com>
2021-10-18 16:08:13 +1100

    adding promises

--- src/Server/LoginHandler.ts
+++ src/Server/LoginHandler.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http"
+import { resolve } from "path/posix";
 
 export class LoginHandler {
     private req: IncomingMessage;
@@ -9,7 +10,28 @@ export class LoginHandler {
         this.res = res;
     }
 
-    public handleRequest():void {
+    public  async handleRequest():Promise<void> {
+       const body = await  this.getRequestBody();
+       console.log(`request username:${body.username}`)
+       console.log(`request password:${body.password}`)
+    }
 
+    private async getRequestBody():Promise<any> {
+        return new Promise((resolve, reject) => {
+            let body = '';
+            this.req.on('data', (data:string)=> {
+                body += data;
+            });
+            this.req.on('end', ()=>{
+                try{
+                    resolve(JSON.parse(body))
+                }catch (error) {
+                    reject(error)
+                }
+            });
+            this.req.on('error', (error:any)=> {
+                reject(error);
+            })
+        });
     }
 }
